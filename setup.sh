bb2b686c90ccdc28db5d510410bcc024d7a6962a
mathe42 <2pi_r2@gmx.de>
2022-10-02 15:23:25 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/EC-Nordbund/next-verwaltung.git verwaltung
+git clone https://github.com/EC-Nordbund/next-leiter.git leiter
+git clone https://github.com/EC-Nordbund/next-login.git login
+git clone https://github.com/EC-Nordbund/next-dev-server.git dev-server
+
+cd verwaltung
+yarn
+cd ../leiter
+yarn
+cd ../login
+yarn
+cd ../dev-server
+yarn
---
+++ start.sh
@@ -0,0 +1,1 @@
+echo "In allen Ordnern 'yarn dev' aufrufen"
